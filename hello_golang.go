0d59421fe99783bc9f876bc8c212a294c79cf9e2
Sakti Tua <saktitua14@gmail.com>
2023-02-16 15:25:48 +0700

    push by sakti

--- hello_golang.go
+++ hello_golang.go
@@ -3,12 +3,13 @@ package hello_golang
 import "fmt"
 
 type HelloGuys struct {
-	Nama    string
-	Age     int
-	Address string
+	Nama         string
+	Age          int
+	Address      string
+	StatusMerrid string
 }
 
 func SayHello() {
-	result := HelloGuys{"sakti", 20, "Medan Indonesia"}
+	result := HelloGuys{"sakti", 20, "Medan Indonesia", "Merrid"}
 	fmt.Println(result)
 }
